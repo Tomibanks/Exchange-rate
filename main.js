1576407d933d080da555af32c3085e5e7e090253
Bankole David <Tomibanks57@gmail.com>
2021-10-01 18:58:48 +0100

    Exchange rate

---
+++ main.js
@@ -0,0 +1,30 @@
+const moneyEl_one = document.getElementById('money-one');
+const moneyEl_two = document.getElementById('money-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+function calculate() {
+ const money_one = moneyEl_one.value;
+ const money_two = moneyEl_two.value;
+}
+
+fetch('https://v6.exchangerate-api.com/v6/7fd01307d9d6ca694b22923d/latest/USD')
+
+.then((res) => res.json())
+.then((data) => {
+ console.log(data);
+ const rate = data.conversion_rates[money_two];
+
+ rateEl.innerText = '1 ${money_one} ${rate} ${money_two}';
+
+})
+
+moneyEl_one.addEventListener('change', calculate);
+moneyEl_two.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+calculate();
